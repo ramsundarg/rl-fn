de85721916340d43e5a4dcb71ec4524a86490038
Ramsundar-Govindarajan_agi <Ramsundar.Govindarajan@allianzgi.com>
2022-11-17 14:16:23 +0100

    Adding server and run_experiment

--- run_experiment.py
+++ run_experiment.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Nov 12 11:41:16 2022
+Created on Mon Nov 14 09:38:02 2022
 
 @author: A00006846
-
 """
+
 import gym
 import tensorflow as tf
 from tensorflow.keras import layers
@@ -23,7 +23,6 @@ import copy
 import os
 from NewBuffer import Buffer
 from mlflow import log_metric, log_param, log_artifact, tensorflow
-import run_experiment
 
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
@@ -96,10 +95,11 @@ ou_noise = OUActionNoise(mean=np.zeros(1), std_deviation=float(std_dev) * np.one
 def trainer(cfg):
     
     total_episodes = cfg['general_settings']['max_episodes']
-    buffer_lib = importlib.import_module('{}'.format(cfg['buffer']['name']))
-    buffer = getattr(buffer_lib, "Buffer")(cfg)
+    buffer = Buffer(cfg)
     env_lib = importlib.import_module('{}'.format(cfg['env']['name']))
     env = getattr(env_lib, cfg['env']['name'])(cfg['env'])
+    buffer_lib = importlib.import_module('{}'.format(cfg['buffer']['name']))
+    buffer = getattr(buffer_lib, "Buffer")(cfg)
     for ep in range(total_episodes):
     
         prev_state = env.reset()
@@ -160,87 +160,3 @@ def run_experiment(cfg):
         mlflow.log_dict(cfg, "cur_cfg.txt")
         trainer(cfg)
         mlflow.end_run()
-
-
-pidx = 0
-pitems = []
-config_count = 0
-def hypertune(level, idx, hypertune_items):
-    global pidx
-    global pitems,config_count,cfg_copy,cfg
-    if idx == len(hypertune_items):
-        if level == 0:
-            cfg_copy.pop('tune', None)
-            run_experiment.run_experiment(cfg_copy)
-            
-            print(cfg_copy)
-            config_count= config_count+1
-            print(config_count)
-            print("\n\n\n")
-            
-        else:
-            hypertune(level-1, pidx+1, pitems)
-        
-        return
-
-    
-    if (list(hypertune_items)[idx]) == "group":
-        for el in list(hypertune_items.values())[idx]:
-            pidx = idx
-            pitems = hypertune_items
-            hypertune(level+1, 0, el)
-            for d in el.keys():
-                keys =d.split('.')
-                c = cfg_copy
-                co = cfg
-                update_key = True
-                for key in keys[0:-1]:
-                    if co.get(key, None) == None:
-                        del c[key] 
-                        update_key = False
-                        break
-                    if c.get(key, None) == None:
-                        c[key] = []
-                    co =co[key]
-                    c= c[key]
-                if update_key:
-                    c[keys[-1]]= co[keys[-1]]
-                
-        return
-    keys = (list(hypertune_items)[idx]).split('.')
-
-    d = cfg_copy
-    for key in keys[0:-1]:
-        if d.get(key, None) == None:
-            d[key] = []
-        d = d.get(key)
-    tune_item = list(hypertune_items.values())[idx]
-    if(tune_item.get("low", "") != ""):
-        is_value = False
-        # print(tune_item["low"],tune_item["high"],tune_item["step"])
-        for val in np.arange(tune_item["low"], tune_item["high"], tune_item["step"]):
-            d[keys[-1]] = val.item()
-            hypertune(level, idx+1, hypertune_items)
-    if(tune_item.get("set")):
-        for val in tune_item["set"]:
-            d[keys[-1]] = eval(val)
-            hypertune(level, idx+1, hypertune_items)
-    if(tune_item.get("list")):
-        for val in tune_item["list"]:
-            d[keys[-1]] = val
-            hypertune(level, idx+1, hypertune_items)
-        
-
-
-
-cfg = {}
-
-file_name = "cfg/temp4.json"
-with open(file_name, "r") as jfile:
-    cfg = json.load(jfile)
-
-cfg_copy = copy.deepcopy(cfg)
-hypertune(0, 0, cfg_copy.get('tune', []))
-
-
-
---
+++ server.py
@@ -0,0 +1,53 @@
+
+import run_experiment
+import multiprocessing
+import json
+from fastapi.templating import Jinja2Templates
+import uvicorn
+from fastapi import FastAPI, Header, Response, Request, Depends
+from fastapi.exceptions import RequestValidationError
+from starlette.responses import JSONResponse
+from starlette.responses import FileResponse 
+from fastapi.staticfiles import StaticFiles
+import time
+import mlflow
+app = FastAPI(
+    title="ML RL",
+    description="ML RL",
+    version="1.0",
+)
+templates = Jinja2Templates(directory="templates")
+
+#app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/") 
+def index(request: Request):
+  return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post('/handle_data')
+async def handle_data(request: Request):
+    form = await request.form()
+    try :
+        cfg = form['op1']
+        cfg = json.loads(cfg)
+        print(cfg)
+        
+        thread = multiprocessing.Process(target= run_experiment.run_experiment,args=(cfg,))
+        thread.start()
+        time.sleep(10)
+        experiment_name = 'Type:{} q:{} a:{}'.format( cfg['env']["U_2"],cfg['ddpg']['q']['name'],cfg['ddpg']['a']['name'])
+        e = mlflow.get_experiment_by_name(experiment_name)
+        df = mlflow.search_runs([e.experiment_id],"attributes.status = 'RUNNING'", order_by=["attribute.start_time DESC"],max_results=1)
+        r = df.iloc[0].run_id
+        
+        
+        return templates.TemplateResponse("index.html", {"request": request,"id" : "Deployed experiment at : http://127.0.0.1:5000/#/experiments/{}/runs/{}".format(int(e.experiment_id),r)})
+    
+    except Exception as e:
+      return templates.TemplateResponse("index.html", {"request": request,"id" : str(e)})
+    #run_experiment(cfg)
+    # your code
+    # return a response
+if __name__ == "__main__":
+    uvicorn.run("server:app", port=50000, log_level="debug")
